dd1ec2d841604fabf7be5337101d1f3d57fdaca6
Chirag Greed <chiraggreed02@gmail.com>
2026-04-02 17:04:31 +0530

    Fix response method in setWildCard to use sendFile for serving index.html

--- Backend/src/controllers/wildCardController.js
+++ Backend/src/controllers/wildCardController.js
@@ -8,6 +8,6 @@ export const setWildCard = async (req, res) => {
 
     const index = path.join(__dirname, "../", "../", "public/dist/index.html");
 
-    res.send(index);
+    res.sendFile(index);
 
 }
